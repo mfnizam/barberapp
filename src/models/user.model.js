788f358a556db79e16835e217532d5fa679fbb90
mfnizam <fatikhunnizam@gmail.com>
2022-09-07 21:17:25 +0700

    feat(account): add users address on users model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -11,6 +11,11 @@ const userSchema = mongoose.Schema(
       required: true,
       trim: true,
     },
+    address: {
+      type: String,
+      required: true,
+      trim: true
+    },
     email: {
       type: String,
       required: true,
--- src/validations/user.validation.js
+++ src/validations/user.validation.js
@@ -6,6 +6,7 @@ const createUser = {
     email: Joi.string().required().email(),
     password: Joi.string().required().custom(password),
     name: Joi.string().required(),
+    address: Joi.string().required(),
     role: Joi.string().required().valid('user', 'admin'),
   }),
 };
@@ -35,6 +36,7 @@ const updateUser = {
       email: Joi.string().email(),
       password: Joi.string().custom(password),
       name: Joi.string(),
+      address: Joi.string(),
     })
     .min(1),
 };
